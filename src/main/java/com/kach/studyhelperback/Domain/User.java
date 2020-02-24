4f2ad7f8b1da92838d0ea425d35a4b187c869f3e
kachmazoff <33556235+kachmazoff@users.noreply.github.com>
2020-02-24 17:47:52 +0400

    Remove application.properties from repo

--- src/main/java/com/kach/studyhelperback/Domain/User.java
+++ src/main/java/com/kach/studyhelperback/Domain/User.java
@@ -1,47 +1,47 @@
-package com.kach.studyhelperback.Domain;
-import javax.persistence.*;
-import java.util.Set;
-@Entity
-@Table(name="usr")
-public class User {
-    @Id
-    private Long id;
-    private String username;
-    private String password;
-    @ElementCollection (targetClass = Role.class, fetch=FetchType.EAGER)//EAGER because in Role only 3 elements
-    @CollectionTable(name="user-role",joinColumns = @JoinColumn(name = "user-id"))//TODO rename column
-    @Enumerated(EnumType.STRING)
-    private Set<Role> roles;
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
+//package com.kach.studyhelperback.Domain;
+//import javax.persistence.*;
+//import java.util.Set;
+//@Entity
+//@Table(name="usr")
+//public class User {
+//    @Id
+//    private Long id;
+//    private String username;
+//    private String password;
+//    @ElementCollection (targetClass = Role.class, fetch=FetchType.EAGER)//EAGER because in Role only 3 elements
+//    @CollectionTable(name="user-role",joinColumns = @JoinColumn(name = "user-id"))//TODO rename column
+//    @Enumerated(EnumType.STRING)
+//    private Set<Role> roles;
+//
+//    public Set<Role> getRoles() {
+//        return roles;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setRoles(Set<Role> roles) {
+//        this.roles = roles;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//}
