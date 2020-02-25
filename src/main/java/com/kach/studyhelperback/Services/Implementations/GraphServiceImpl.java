7fa7b796eea65860760dac913bd7f793481417b9
kachmazoff <33556235+kachmazoff@users.noreply.github.com>
2020-02-25 13:21:48 +0400

    Add Graph Service

---
+++ src/main/java/com/kach/studyhelperback/Services/Implementations/GraphServiceImpl.java
@@ -0,0 +1,18 @@
+package com.kach.studyhelperback.Services.Implementations;
+
+import com.kach.studyhelperback.Models.DAO.Article;
+import com.kach.studyhelperback.Services.Definitions.GraphService;
+
+import java.util.List;
+
+public class GraphServiceImpl implements GraphService {
+    @Override
+    public void topSort() {
+
+    }
+
+    @Override
+    public List<Article> findPath(Integer startId, Integer endId) {
+        return null;
+    }
+}
