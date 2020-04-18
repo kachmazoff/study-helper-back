ef20944af07ddf43514664e9d1ded5b020544076
Andrei Riazanov <Apple556@yandex.ru>
2020-04-18 17:39:01 +0400

    Add ArticleComments and repo for it

---
+++ src/main/java/com/kach/studyhelperback/model/ArticleComments.java
@@ -0,0 +1,43 @@
+package com.kach.studyhelperback.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "article_comments")
+public class ArticleComments extends BaseEntity {
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "article_id", nullable = false)
+    Article article;
+
+    @NotNull
+    String text;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "reply_comment_id", nullable = false)
+    ArticleComments replyComment;
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public ArticleComments getReplyComment() {
+        return replyComment;
+    }
+
+    public void setReplyComment(ArticleComments replyComment) {
+        this.replyComment = replyComment;
+    }
+}
