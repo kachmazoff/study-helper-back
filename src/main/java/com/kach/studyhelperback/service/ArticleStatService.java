009617842f33cab462891103e71074c423903c6b
kachmazoff <33556235+kachmazoff@users.noreply.github.com>
2020-04-20 16:53:42 +0400

    Small fix

--- src/main/java/com/kach/studyhelperback/service/ArticleStatService.java
+++ src/main/java/com/kach/studyhelperback/service/ArticleStatService.java
@@ -1,11 +1,8 @@
 package com.kach.studyhelperback.service;
 
-import org.springframework.stereotype.Service;
-
 import java.util.Date;
 import java.util.HashMap;
 
-@Service
 public interface ArticleStatService {
     Long getArticleStat (Long articleId);
 //    Long getArticlesStat();
